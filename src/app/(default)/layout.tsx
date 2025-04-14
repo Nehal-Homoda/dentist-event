6e36f8e32fcd87462c73952d9304665aff1d4433
unknown <nehal>
2025-04-14 16:10:16 +0200

    first commit

---
+++ src/app/(default)/layout.tsx
@@ -0,0 +1,17 @@
+import AppFooter from "@/components/layout/AppFooter";
+import AppNavBar from "@/components/layout/AppNavBar";
+import React from "react";
+
+export default function defaultlayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div>
+            <AppNavBar />
+            {children}
+            <AppFooter />
+        </div>
+    );
+}
---
+++ src/components/layout/AppNavBar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AppNavBar() {
+    return (
+        <div className="bg-error">
+            <p className="text-error bg-primary">biuaygd</p>
+        </div>
+    );
+}
