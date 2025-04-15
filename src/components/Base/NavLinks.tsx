ad5bb2f5b94ee712c43082e9b4683b99039ab44f
unknown <nehal>
2025-04-15 16:26:23 +0200

    make navBar showing sponsor dynamically

--- src/components/Base/NavLinks.tsx
+++ src/components/Base/NavLinks.tsx
@@ -2,7 +2,8 @@ import Link from "next/link";
 import React from "react";
 import { usePathname } from "next/navigation";
 import RoutesManager from "@/core/RoutesManager";
-
+import { brand, elements } from "@/core/AssetsManager";
+import Button from "../Base/button";
 
 const navLinks = [
     { name: "Home", link: RoutesManager.home },
@@ -15,30 +16,59 @@ const navLinks = [
     { name: "profile", link: RoutesManager.profile },
 ];
 
-type Props={
-    colorInverted:Boolean
-}
-export default function NavLinks({colorInverted}:Props) {
+type Props = {
+    colorInverted: Boolean;
+    isWithSponsors: Boolean;
+};
+
+const sponsorsImg = [elements.sponsor1, elements.sponsor2, elements.sponsor3];
+export default function NavLinks({ colorInverted, isWithSponsors }: Props) {
     const pathName = usePathname();
     return (
-        <div
-            className={` ${
-                colorInverted ? "bg-primary-light" : "bg-white"
-            }  text-background rounded-md flex justify-between gap-12  px-12 py-4`}
-        >
-            {navLinks.map((item, index) => (
-                <Link
-                    key={index}
-                    href={item.link}
-                    className={
-                        item.link == pathName
-                            ? "relative before:absolute before:w-full before:content-[''] before:bg-background  before:h-1  before:top-7 "
-                            : "bg-yellow"
-                    }
-                >
-                    {item.name}
-                </Link>
-            ))}
-        </div>
+        <>
+            {isWithSponsors && (
+                <div className="w-full bg-background flex justify-between items-center  px-12 ">
+                    <div className="w-24 h-24">
+                        <img
+                            src={brand.logo_text.src}
+                            className="w-full h-full object-contain"
+                            alt=""
+                        />
+                    </div>
+
+                    {sponsorsImg.map((item, index) => (
+                        <div className="w-28 h-28 ">
+                            <img
+                                className="w-full h-full object-contain"
+                                src={item.src}
+                                alt=""
+                            />
+                        </div>
+                    ))}
+                    <Button isWithArrow={false}>
+                        <span>Register Now !</span>
+                    </Button>
+                </div>
+            )}
+            <div
+                className={` ${
+                    colorInverted ? "bg-primary-light" : "bg-white"
+                }  text-background rounded-md flex justify-between gap-12  px-12 py-4`}
+            >
+                {navLinks.map((item, index) => (
+                    <Link
+                        key={index}
+                        href={item.link}
+                        className={
+                            item.link == pathName
+                                ? "relative before:absolute before:w-full before:content-[''] before:bg-background  before:h-1  before:top-7 "
+                                : "bg-yellow"
+                        }
+                    >
+                        {item.name}
+                    </Link>
+                ))}
+            </div>
+        </>
     );
 }
--- src/components/layout/AppNavBar.tsx
+++ src/components/layout/AppNavBar.tsx
@@ -1,40 +1,17 @@
 "use client";
 
 import React from "react";
-import { brand, elements } from "@/core/AssetsManager";
-import NavLinks from "../Base/NavLinks";
-import Button from "../Base/button";
 
-const sponsorsImg = [elements.sponsor1, elements.sponsor2, elements.sponsor3];
+import NavLinks from "../Base/NavLinks";
+
+
 
 export default function AppNavBar() {
     return (
         <>
             <nav className=" absolute">
-                <div className="w-full bg-background flex justify-between items-center  px-12 ">
-                    <div className="w-24 h-24">
-                        <img
-                            src={brand.logo_text.src}
-                            className="w-full h-full object-contain"
-                            alt=""
-                        />
-                    </div>
-
-                    {sponsorsImg.map((item, index) => (
-                        <div className="w-28 h-28 ">
-                            <img
-                                className="w-full h-full object-contain"
-                                src={item.src}
-                                alt=""
-                            />
-                        </div>
-                    ))}
-                    <Button isWithArrow={false}>
-                        <span>Register Now !</span>
-                    </Button>
-                </div>
-                <NavLinks colorInverted={true} />
-                <input className="outline-input" placeholder="nehe"/>
+               
+                <NavLinks isWithSponsors={true} colorInverted={true} />
             </nav>
         </>
     );
