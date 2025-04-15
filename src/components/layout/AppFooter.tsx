40db8e84bcb70339c82ce23bcb150e8af0dc2fc0
Mohamed Elsayed <jascriptor@gmail.com>
2025-04-15 15:11:58 +0200

    update

--- src/components/layout/AppFooter.tsx
+++ src/components/layout/AppFooter.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from "react";
+import { bg, brand } from "@/core/AssetsManager";
 
 export default function AppFooter() {
-  return (
-    <div></div>
-  )
+    return (
+        <>
+            <div className="container pb-10">
+                <footer
+                    className="text-white w-full py-10 px-5 bg-cover rounded-t-2xl"
+                    style={{
+                        background: `url('${bg.footer_bg.src}') no-repeat top`,
+                    }}
+                >
+                    <div className="grid grid-cols-10">
+                        <div className="brand col-span-1">
+                            <img
+                                src={brand.logo.src}
+                                className="w-full max-h-[200px] object-contain"
+                                alt=""
+                            />
+                        </div>
+                        <div className="details">
+                            <h5 className="text-white text-lg">Location</h5>
+                            <p className="text-sm">Egypt, Cairo</p>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </>
+    );
 }
---
+++ src/core/AssetsManager.ts
@@ -0,0 +1,19 @@
+//========= [BRAND]
+import logo from '@/assets/images/logo.png';
+import logo_text from '@/assets/images/logo-text.png';
+
+export const brand = {
+    logo,
+    logo_text
+};
+//========= [ICONS]
+export const icons = {};
+//========= [BG]
+import footer_bg from '@/assets/images/bg/footer-bg.png';
+export const bg = {
+    footer_bg
+};
+//========= [ELEMENTS]
+export const elements = {};
+//========= [HEROS]
+export const heros = {};
