71187205303a77d792b450a3951d6ee70610fe7f
unknown <nehal>
2025-04-15 18:53:17 +0200

    update

---
+++ src/app/(profile)/layout.tsx
@@ -0,0 +1,18 @@
+import AppFooter from '@/components/layout/AppFooter'
+import AppNavBar from '@/components/layout/AppNavBar'
+import React from 'react'
+
+
+
+type Props={
+    children:React.ReactNode
+}
+export default function profilelayout({children}:Props) {
+  return (
+    <div>
+        <AppNavBar isWithSponsors={false} colorInverted={true}></AppNavBar>
+        {children}
+        <AppFooter/>
+    </div>
+  )
+}
