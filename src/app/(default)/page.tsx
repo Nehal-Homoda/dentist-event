709b7d2ea0b4be68031bf50b845aef7814ecc5ff
Mohamed Elsayed <jascriptor@gmail.com>
2025-04-16 14:38:30 +0200

    feat: finish whySubmitSection

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -1,9 +1,15 @@
+import WhySubmitSection from "@/components/static/WhySubmitSection";
 
 export default function Home() {
-  return (
-    <div className="w-full aspect-[1.5/1] bg-[url('@/assets/images/bg/header.png')] bg-no-repeat bg-cover bg-top">
+    return (
+        <>
+            <div className="home-page">
+                <div className="w-full aspect-[1.5/1] bg-[url('@/assets/images/bg/header.png')] bg-no-repeat bg-cover bg-top"></div>
 
-   
-    </div>
-  );
+                <div className="container">
+                    <WhySubmitSection></WhySubmitSection>
+                </div>
+            </div>
+        </>
+    );
 }
---
+++ src/components/static/WhySubmitSection.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import BaseBtn from "../Base/BaseBtn";
+import { elements, heros } from "@/core/AssetsManager";
+
+export default function WhySubmitSection() {
+    return (
+        <>
+            <div className="py-20 relative">
+                <img
+                    src={elements.logo_year.src}
+                    alt=""
+                    className="absolute -top-10 left-[50%] translate-x-[-50%]"
+                />
+                <div className="grid grid-cols-1 md:grid-cols-5 relative gap-16 md:gap-16 lg:gap-28 items-center">
+                    <div className="md:col-span-2 pt-10 relative flex  md:justify-end">
+                        <div className="hero-card w-full max-w-[300px] md:max-w-[350px] aspect-[3/3.25]  relative">
+                            <img
+                                src={elements.card_frame.src}
+                                alt=""
+                                className="w-full h-full object-contain absolute -right-[3rem] -top-[2rem]"
+                            />
+                            <img
+                                src={heros.why_submit.src}
+                                alt=""
+                                className="w-full h-full object-cover rounded-2xl overflow-hidden relative"
+                            />
+                        </div>
+                    </div>
+                    <div className="md:col-span-3">
+                        <p className=" text-sm font-semibold">Why Submit ?</p>
+                        <h3 className="text-2xl md:text-3xl mt-5 mb-5">
+                            Lorem Ipsum is
+                            <br />
+                            simply dummy text of printing
+                        </h3>
+
+                        <p className="text-muted text-justify mb-10 md:pe-10">
+                            Lorem Ipsum  is simply dummy text of the printing
+                            and typesetting industry . c Lorem Ipsum has been
+                            the industry's standard dummy text ever since the
+                            1500s, when an unknown printer took a galley of type
+                            and scra
+                        </p>
+
+                        <BaseBtn>Register now</BaseBtn>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
