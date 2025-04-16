7ba9e05834c09e653b032c5150d9649b03ed7ff4
Mohamed Elsayed <jascriptor@gmail.com>
2025-04-16 13:06:05 +0200

    feat: finish footer, enhance BaseBtn

--- src/components/Base/BaseBtn.tsx
+++ src/components/Base/BaseBtn.tsx
@@ -1,17 +1,16 @@
 import React from "react";
 type Props = {
     children: React.ReactNode;
-    isWithArrow?:Boolean 
+    isWithArrow?:Boolean;
+    minWidth?: string 
 };
-export default function BaseBtn({ children ,isWithArrow=true}: Props) {
+export default function BaseBtn({ children ,isWithArrow=true, minWidth = ''}: Props) {
     return (
-        <div className="">
-            <button className="bg-gradient-to-r from-primary-100 to-primary-light-100 rounded-md text-background  font-bold px-4 h-10">
+            <button className={`flex-grow-0 flex-shrink-0 bg-gradient-to-r from-primary to-primary-light rounded-full text-background  font-semibold px-4 text-sm h-10 ${minWidth}`}>
                 {children}
                 {isWithArrow && (
-                    <span className="ms-2 font-bold mdi mdi-arrow-right"></span>
+                    <span className="ms-2 mdi mdi-arrow-right"></span>
                 )}
             </button>
-        </div>
     );
 }
--- src/components/layout/AppFooter.tsx
+++ src/components/layout/AppFooter.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { bg, brand } from "@/core/AssetsManager";
+import BaseBtn from "../Base/BaseBtn";
 
 export default function AppFooter() {
     return (
         <>
             <div className="container pb-10">
                 <footer
-                    className="text-white w-full py-10 px-5 bg-cover rounded-t-2xl"
+                    className="text-white w-full bg-cover px-5 rounded-t-2xl"
                     style={{
                         backgroundImage: `url('${bg.footer_bg.src}')`,
-                        backgroundPosition: 'center top'
+                        backgroundPosition: "center top",
                     }}
                 >
-                    <div className="grid grid-cols-1 md:grid-cols-10">
+                    <div className="grid grid-cols-1 md:grid-cols-10 py-16">
                         <div className="brand col-span-1">
                             <img
                                 src={brand.logo.src}
@@ -71,20 +72,31 @@ export default function AppFooter() {
                             </div>
                         </div>
                         {/* <div className="p-5 col-span-1"></div> */}
-                        <div className="md:col-span-4">
+                        <div className="md:col-span-4 mt-10">
                             <h5 className="text-white font-semibold mb-5">
-                            Stay in touch with us 
+                                Stay in touch with us
                             </h5>
                             <p className="text-xs pe-10 leading-6">
-                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has  the industry's standard 
+                                Lorem Ipsum is simply dummy text of the printing
+                                and typesetting industry. Lorem Ipsum has the
+                                industry's standard
                             </p>
 
-                            <div className="input-wrap flex">
-                              <input type="text" /> 
-                              <button>dsfds</button>
-
+                            <div className="input-wrap flex gap-5 mt-12">
+                                <input
+                                    type="text"
+                                    placeholder="Enter your email"
+                                    className="w-full rounded-full placeholder:text-xs text-foreground outline-none border-none px-5"
+                                />
+                                <BaseBtn minWidth="min-w-[5rem]">
+                                    Send Now
+                                </BaseBtn>
                             </div>
                         </div>
+                    </div>
+                    <div className="divider w-full h-[0.01rem] bg-white/30"></div>
+                    <div className="copyright py-3 text-center text-sm">
+                    © {new Date().getFullYear()} <a className="text-primary font-bold" href="/">EAOMS</a>. All rights reserved
                     </div>
                 </footer>
             </div>
