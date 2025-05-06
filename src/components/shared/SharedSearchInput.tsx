20190ad8c976eafaaa520db7c94149c78c25c02b
nehal <nehalhomoda00@gmail.com>
2025-05-06 04:40:42 +0300

    work on search input

--- src/components/shared/SharedSearchInput.tsx
+++ src/components/shared/SharedSearchInput.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
 
-export default function SharedSearchInput() {
-    const arr = ['mahmoud', 'ahmed', 'nehal', 'aml', 'ibrahim']
+
+type Props = {
+    inputValue: string
+    sendValueToParent: (e: React.ChangeEvent<HTMLInputElement>) => void
+    searchAction: () => void
+}
+
+export default function SharedSearchInput({ inputValue, sendValueToParent, searchAction }: Props) {
     return (
         <div>
             <div className='relative'>
-                <input className='px-5 py-2 rounded-lg w-full text-sm focus:outline-none text-secondary-100 ' placeholder='search here' type="text" />
-                <span className="text-xl mdi mdi-magnify absolute text-primary  right-2 top-1/2 -translate-y-1/2"></span>
-                <div className=''>
-                    {arr.map((item, index) => (<span>{item}</span>))}
-                </div>
+                <input value={inputValue} onChange={sendValueToParent} className='px-5 py-2 rounded-lg w-full text-sm focus:outline-none text-secondary-100 ' placeholder='search here' type="text" />
+                
+                <button className='border-none text-xl absolute text-primary  right-2 top-1/2 -translate-y-1/2' onClick={searchAction}>
+
+                <span  className="mdi mdi-magnify "></span>
+
+
+                </button>
+
 
             </div>
         </div>
